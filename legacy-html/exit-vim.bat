9485cd98f2daf288f465480137f939e67a6869b2
kakporosaire953-creator <kakporosaire953@gmail.com>
2026-05-16 10:29:44 +0200

    Remove legacy html and css files to keep repo clean

--- legacy-html/exit-vim.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Resolving git merge conflict...
-echo q! > .git/MERGE_MSG
-git merge --abort
-echo Git merge conflict resolved.
--- legacy-html/js/remove_numbers.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('index.html', 'utf8');
-
-// Regex to remove sequence numbers
-const regex = /<div class="(problem|layer|step)-number[^"]*">[^<]+<\/div>/g;
-content = content.replace(regex, '');
-
-fs.writeFileSync('index.html', content, 'utf8');
-console.log('Successfully removed sequence numbers from index.html without corrupting encoding.');
